e98e57c2458ab1eb62dc9ac856d23230b5e56bc5
mgonzales-rs <melissa.gonzales@riversafe.co.uk>
2022-01-25 01:24:31 +0000

    adding test ci

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,13 @@
 cp -r . /data
 cd /data
 
+echo "" >> .ezerc.toml
+echo "" >> .ezerc.toml
 echo "[sarif.github]" >> .ezerc.toml
 echo "REPORT_FILE = \"reports/$INPUT_SARIF_FILE\"" >> .ezerc.toml
 echo "" >> .ezerc.toml
 echo "[scan.github]" >> .ezerc.toml
-echo "reporters = [\"sarif\"]" >> .ezerc.toml
+echo "reporters = [\"sarif.github\"]" >> .ezerc.toml
 echo "" >> .ezerc.toml
 
 cat .ezerc.toml
