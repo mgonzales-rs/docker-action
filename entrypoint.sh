d2487cc7a5624ec04681a4f8c1e294154e4f4e75
mgonzales-rs <melissa.gonzales@riversafe.co.uk>
2022-01-25 00:56:18 +0000

    adding test ci

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,10 @@ echo "REPORT_FILE = \"reports/eze_report.sarif\"" >> .ezerc.toml
 echo "" >> .ezerc.toml
 echo "[scan.github]" >> .ezerc.toml
 echo "reporters = [\"sarif\"]" >> .ezerc.toml
+echo "" >> .ezerc.toml
+echo "[scan.github]" >> .ezerc.toml
+echo "tools = [\"python-piprot\", \"python-bandit\", \"python-safety\", \"python-cyclonedx\", \"trufflehog\"]" >> .ezerc.toml
+echo "" >> .ezerc.toml
 cat .ezerc.toml
 eze test -s github
 cat /data/reports/eze_report.sarif
