19e3893b28ec9619ebf3a337762722240074134b
mgonzales-rs <melissa.gonzales@riversafe.co.uk>
2022-01-27 02:18:55 +0000

    modify yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,5 +16,5 @@ cat .ezerc.toml
 
 eze test -s github
 cat reports/$INPUT_SARIF_FILE
-# cp reports/$INPUT_SARIF_FILE /github/workspace/$INPUT_SARIF_FILE
+cp reports/$INPUT_SARIF_FILE /github/workspace/$INPUT_SARIF_FILE
 echo ::set-output name=$INPUT_SARIF_FILE::$(cat reports/$INPUT_SARIF_FILE)
