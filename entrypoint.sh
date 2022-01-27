f3ad86c0a0f55cb3ddd2edd3e460279eb585833b
mgonzales-rs <melissa.gonzales@riversafe.co.uk>
2022-01-27 02:32:51 +0000

    modify yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,6 @@ echo "[scan.github]" >> .ezerc.toml
 echo "reporters = [\"sarif:github\"]" >> .ezerc.toml
 echo "" >> .ezerc.toml
 
-cat .ezerc.toml
 
 eze test -s github
-cat reports/$INPUT_SARIF_FILE
-cp reports/$INPUT_SARIF_FILE /github/workspace/$INPUT_SARIF_FILE
-echo 'sarif_file<<EOT' >> $GITHUB_ENV
-cat reports/$INPUT_SARIF_FILE >> $GITHUB_ENV
-echo 'EOT' >> $GITHUB_ENV
+echo ::set-output name=sarif_file::$(cat reports/$INPUT_SARIF_FILE)
