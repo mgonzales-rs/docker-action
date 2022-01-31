63e5c71be636a9081028a11102596aa6ae7b2b8a
mgonzales-rs <melissa.gonzales@riversafe.co.uk>
2022-01-31 11:52:46 +0000

    Implement markdown in python entrypoint

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,27 @@
 import os
 
-if os.environ.get("INPUT_UPLOAD_METHOD"):
+toml = open(".ezerc.toml", "a")
+
+if os.environ.get("INPUT_UPLOAD_METHOD") == "sarif":
     print("ok")
 else:
-    print("nok")
+    print("printing in MARKDOWN")
+    toml.write(
+        """
+    [markdown.github]
+    REPORT_FILE = \"reports/eze.md\"
+
+    [scan.github]
+    reporters = [\"markdown:github\"]
+    """
+    )
+    os.system("eze test -s github")
+    markdown = file_object = open("reports/eze.md", "r")
+    content = "\n".join(markdown.readlines())
+    markdown.close()
+    content.replace("%", "%25")
+    content.replace("\n", "%0A")
+    content.replace("\r", "%0D")
+    print(f"::set-output name=markdown_file::{content}")
+
+toml.close()
