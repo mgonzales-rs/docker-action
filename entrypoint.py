59a153ef1f828a7645e300ea34456c9bb9ee305d
mgonzales-rs <melissa.gonzales@riversafe.co.uk>
2022-01-31 12:04:08 +0000

    ok

--- entrypoint.py
+++ entrypoint.py
@@ -4,8 +4,32 @@ import os
 os.system("cp -r . /data")
 os.chdir("/data")
 
+
+def replace_special_chararacters(content):
+    content = content.replace("%", "%25")
+    content = content.replace("\n", "%0A")
+    content = content.replace("\r", "%0D")
+    return content
+
+
 if os.environ.get("INPUT_UPLOAD_METHOD") == "sarif":
-    print("ok")
+    print("printing in Sarif")
+    toml = open("./.ezerc.toml", "a")
+    toml.write(
+        """
+    [sarif.github]
+    REPORT_FILE = \"reports/eze.sarif\"
+
+    [scan.github]
+    reporters = [\"sarif:github\"]
+    """
+    )
+    toml.close()
+    os.system("eze test -s github")
+    markdown = file_object = open("reports/eze.sarif", "r")
+    content = replace_special_chararacters("\n".join(markdown.readlines()))
+    markdown.close()
+    print(f"::set-output name=sarif_file::{content}")
 else:
     print("printing in MARKDOWN")
     toml = open("./.ezerc.toml", "a")
@@ -21,9 +45,5 @@ else:
     toml.close()
     os.system("eze test -s github")
     markdown = file_object = open("reports/eze.md", "r")
-    content = "\n".join(markdown.readlines())
-    markdown.close()
-    content = content.replace("%", "%25")
-    content = content.replace("\n", "%0A")
-    content = content.replace("\r", "%0D")
+    content = replace_special_chararacters("\n".join(markdown.readlines()))
     print(f"::set-output name=markdown_file::{content}")
